5c0af7d02bbf89bc0d2df3b5692d5d3089c9851c
SuhelKhan <Suhel7822@gmail.com>
2023-04-21 14:14:01 +0530

    changed projects and questions

--- src/components/about/aboutBox.jsx
+++ src/components/about/aboutBox.jsx
@@ -7,7 +7,7 @@ const AboutBox = () => {
         <i className="about-icon icon-fire fire-project"></i>
 
         <div>
-          <h3 className="about-title">5+</h3>
+          <h3 className="about-title">3</h3>
           <span className="aboyt-subtitle">Projects</span>
         </div>
       </div>
@@ -16,7 +16,7 @@ const AboutBox = () => {
         <i className="about-icon icon-badge badge-question"></i>
 
         <div>
-          <h3 className="about-title">150+</h3>
+          <h3 className="about-title">70+</h3>
           <span className="aboyt-subtitle">Questions</span>
         </div>
       </div>
